b833fe7f9ee7c838a80e1435f1cd934584621673
jtontiwith <jtonti@gmail.com>
2019-09-11 11:36:14 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::API
+  before_action :underscore_params!
+  protect_from_forgery with: :null_session
+  respond_to :json
+  
+
+
+
+
+
+  private
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
+
+
+
+end
